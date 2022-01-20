35159f2cc4605a8842a99525ffbc0e561d238215
sheila <sheilapg26@gmail.com>
2022-01-20 16:11:56 +0100

    Cart

--- js/shop.js
+++ js/shop.js
@@ -4,55 +4,64 @@ var products = [
         id: 1,
         name: 'Avocado',
         price: 10.5,
-        type: 'grocery'
+        type: 'grocery',
+        img: './images/agucate.jpeg'
     },
     {
         id: 2,
         name: 'Tomato',
         price: 6.25,
-        type: 'grocery'
+        type: 'grocery',
+        img:'./images/tomate-cana-andaluz.jpeg'
     },
     {
         id: 3,
         name: 'Banana',
         price: 5,
-        type: 'grocery'
+        type: 'grocery',
+        img: './images/banana_64728013.jpeg'
     },
     {
         id: 4,
         name: 'Lip stick',
         price: 260,
-        type: 'beauty'
+        type: 'beauty',
+        img:'./images/pintalabios-logona-45-g.jpeg'
     },
     {
         id: 5,
         name: 'Makeup',
         price: 20.5,
-        type: 'beauty'
+        type: 'beauty',
+        img: './images/1616669821-base-maquillaje-sephora-1616669812.jpeg'
     },
     {
         id: 6,
         name: 'Eyeliner',
         price: 12.75,
-        type: 'beauty'
+        type: 'beauty',
+        img: './images/d8af9c0020873d1981638ffa33afefcd1a6d9e456f6251718a01f55101dbf5a307bab8070459fd9e56034e81f94b5a04d8545dcd21569c123f02d48da32809fc_1.jpeg'
     },
     {
         id: 7,
         name: 'Dress',
         price: 15,
-        type: 'clothes'
+        type: 'clothes',
+        img: './images/71vJ6Jj-IqL._AC_UL1500_.jpg'
     },
     {
         id: 8,
         name: 'Shorts',
         price: 19.99,
-        type: 'clothes'
+        type: 'clothes',
+        img: './images/da8bb419e9a7dc5a2b27040990508a9d.jpeg'
     },
     {
         id: 9,
         name: 'Skirt',
         price: 9.99,
-        type: 'clothes'
+        type: 'clothes',
+        img: './images/614tCTX0LeL._AC_UX385_.jpg'
     }
 ]
 // Array with products (objects) added directly with push(). Products in this array are repeated.
@@ -105,9 +114,12 @@ function applyPromotionsCart() {
     for (var i = 0; i < cart.length; i++) {
         if (cart[i].id == 1 && cart[i].quantity >= 3) {
             cart[i].price = 10;
-            console.log("precio", cart[i].price)
+        } else {
+            cart[i].price = 10.5;
         } if (cart[i].id == 3 && cart[i].quantity >= 10) {
             cart[i].price = cart[i].price / 3;
+        } else {
+            cart[i].price = 5;
         }
     }
 }
@@ -133,7 +145,6 @@ function addToCart(id) {
             }
         }
     })
-    console.log(cart);
     printCart()
 }
 
@@ -143,15 +154,12 @@ function removeFromCart(id) {
         if (cart[i].id == id) {
             if (cart[i].quantity > 1) {
                 cart[i].quantity--;
-                console.log("cantidad", cart)
             } else {
                 cart = cart.filter(function (product) {
                     return product.id !== cart[i].id
                 })
-                console.log("solo1", cart)   
             }       
         }
-        break;
     }   
     printCart();
 }
@@ -159,13 +167,13 @@ function removeFromCart(id) {
 // Exercise 9
 function printCart() {
     var p = document.createElement("p");
-    p.innerHTML = `<p>Total precio: ${calculateTotal()}<p> `
+    p.innerHTML = `<p class="totalPriceCart">Total price: ${calculateTotal()}$<p> `
     
     var list = document.querySelector('.list');
     list.innerHTML = "";
     cart.forEach((product) => {
         var li = document.createElement("li");
-        li.innerHTML = `<li><p>Artículo: </p>${product.name} <p>Precio: </p>${product.price} <p>Cantidad: </p>${product.quantity} <p>Eliminar: </p><button type="button" onClick="removeFromCart(${product.id})">X</button></li>`
+        li.innerHTML = `<li><img class="productImgCart" src=${product.img} alt=${product.name} ><span class="productCart">${product.name}:</span> <p class="quantityCart">Quantity: <span>${product.quantity}</span></p> <p class="priceCart">Price: <span>${product.price}$</span></p> <button type="button" onClick="removeFromCart(${product.id})" class="buttonCart">X</button></li>`
         list.appendChild(li); 
     })
 
